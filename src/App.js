47a63ae1093ec6d02fd6d488772e4edcd0009b3c
ksenyshutova <shutovasn@mail.ru>
2024-06-11 14:02:57 +0300

    first task

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import ShopItemFunc from './ShopItemFunc';
+import './App.css';
+import { item } from './item';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
